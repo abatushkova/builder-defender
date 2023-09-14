c2347747cbefca44426ff8f1749e9db7e2c25edc
Alena Batushkova <alena.batushkova@gmail.com>
2023-09-14 14:47:33 +0300

    fix resources-ui offset

--- Assets/Scripts/ResourcesUI.cs
+++ Assets/Scripts/ResourcesUI.cs
@@ -24,7 +24,7 @@ public class ResourcesUI : MonoBehaviour
             Transform resourceTransform = Instantiate(resourceTemplate, transform);
             resourceTransform.gameObject.SetActive(true);
 
-            float offsetAmount = -160f;
+            float offsetAmount = 140f;
             resourceTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(offsetAmount * index, 0);
             resourceTransform.Find("image").GetComponent<Image>().sprite = resourceType.sprite;
 
